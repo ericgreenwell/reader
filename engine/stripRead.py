452c8a4798203cb16af7cbf691f2fc972e2fba20
ericgreenwell <ericgreenwell2@gmail.com>
2018-12-13 15:10:58 -0500

    Update stripRead.py

--- engine/stripRead.py
+++ engine/stripRead.py
@@ -1,6 +1,13 @@
 
 import cv2
 import numpy as np
+import peakutils
+import matplotlib
+import sys
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+plt.switch_backend('agg')
+from scipy.signal import find_peaks_cwt
 
 im = cv2.imread("whole.jpg",0)
 
@@ -21,6 +28,33 @@ ax[0].plot(baseline_vals)
 ax[0].set_ylabel('8-bit Val')
 ax[0].grid(True)
 
+cleanDat= avg-baseline_vals
+indices=peakutils.indexes(cleanDat, thres=0.5, min_dist=30)
+peaks = cleanDat[indices]
+  
+ax[1].plot(indices, peaks, 'x')
+ax[1].plot(cleanDat)
+ax[1].set_xlabel('pixel Index')
+ax[1].set_ylabel('8-bit Value')
+ax[1].grid(True)
+
 cv2.imshow("clipped",equal)
 cv2.waitKey(0)
+
+if len(indices) == 2:
+  print('matched')
+  rat = peaks[0]/peaks[-1]
+  if rat > 0.6:
+    res = 'Postitive'
+  if 0.3 <= rat <= 0.6:
+    res = 'Weak Positive'
+
+elif len(indices) == 1:
+  res= 'Negative'
+  rat = 0
+
+print("finished")
+
+sys.stdout.flush()
+
 
